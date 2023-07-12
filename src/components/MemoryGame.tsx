474715492c3e49de970bbaea68ffc59bf07b81f2
A1exsandro <alexsandro.acp@gmail.com>
2023-07-12 19:56:10 -0400

    feat: use cards from useMemory in MemoryGame

--- src/components/MemoryGame.tsx
+++ src/components/MemoryGame.tsx
@@ -2,14 +2,18 @@ import Card from './Card'
 import { useMemory } from '../contexts/MemoryContext'
 
 const MemoryGame = () => {
-  const { } = useMemory()
+  const { cards } = useMemory(); console.log(cards)
 
   return (
     <div className="p-2">
       <h1>Memory Game</h1>
 
       <div className="grid grid-cols-4 gap-2">
-        <Card />
+        {
+          cards.map((card, i) => (
+            <Card key={i} {...card}/>
+          ))
+        }
       </div>
     </div>
   )
