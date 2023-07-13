9b98d98de3038176774f979430b125eef8a2d122
A1exsandro <alexsandro.acp@gmail.com>
2023-07-13 17:48:25 -0400

    feat: loading cards in component memory

--- src/components/MemoryGame.tsx
+++ src/components/MemoryGame.tsx
@@ -1,14 +1,20 @@
 import Card from './Card'
-import { useMemory } from '../contexts/MemoryContext'
+import { useMemory } from '../contexts/MemoryContext' 
 
-const MemoryGame = () => {
-  const { cards } = useMemory(); console.log(cards)
+const MemoryGame = () => { 
+  const { cards, startGame } = useMemory()
 
   return (
-    <div className="p-2">
+    <div className="p-3">
       <h1>Memory Game</h1>
+      <button
+        className="bg-black text-white cursor-pointer px-4"
+        onClick={startGame}
+      >
+        Start
+      </button>
 
-      <div className="grid grid-cols-4 gap-2">
+      <div className="grid grid-cols-4 gap-3">
         {
           cards.map((card, i) => (
             <Card key={i} {...card}/>
